4776e965c2907cad9a228bf54e3173ec6da283eb
Marino Dzalto <126403059+Marino-Dzalto@users.noreply.github.com>
2023-03-25 16:37:38 +0100

    Add files via upload

---
+++ constante.java
@@ -0,0 +1,36 @@
+public class constante {
+
+	 String EPSILON = "$";
+	 String EMPTY_STATE = "#";
+	 String COMMA_SEPARATOR = ",";
+	 String VERTICAL_BAR_SEPARATOR = "|";
+	 String SPLITER = "->";	 
+//	 String[] parts;
+	 
+//		// Define a TreeMap 'map' to store state transitions
+//	 Map<String, Set<String>> map = new TreeMap<>();
+	
+	public String getEPSILON() {
+		return EPSILON;
+	}
+	public String getEMPTY_STATE() {
+		return EMPTY_STATE;
+	}
+	public String getCOMMA_SEPARATOR() {
+		return COMMA_SEPARATOR;
+	}
+	public String getVERTICAL_BAR_SEPARATOR() {
+		return VERTICAL_BAR_SEPARATOR;
+	}
+	public String getSPLITER() {
+		return SPLITER;
+	}
+//	public String[] getParts() {
+//		return parts;
+//	}
+
+//	public Map<String, Set<String>> getMap() {
+//		return map;
+//	}
+	
+}
